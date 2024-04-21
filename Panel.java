a9db0633579d779b8a025c4b30c2dd32ab40f743
ashwinbmhadeshwar <142456220+ashwinbmhadeshwar@users.noreply.github.com>
2024-04-21 11:18:57 -0400

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -1,1 +1,35 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+//must 'extend' JPanel 
+//You can rename the class to anything you wish - default is 'PanelTemplate'
+public class Panel extends JPanel
+{
+	//variables for the overall width and height
+	private int w, h;
+	
+	//sets up the initial panel for drawing with proper size
+	public Panel(int w, int h)
+	{
+		this.w = w;
+		this.h = h;
+		this.setPreferredSize(new Dimension(w,h));
+		
+	}
+	
+	
+	//all graphical components go here
+	//this.setBackground(Color c) for example will change background color
+	public void paintComponent(Graphics g)
+	{
+		//this line sets up the graphics - always needed
+		super.paintComponent(g);
+		
+		//all drawings below here:
+		
+		
+		
+	}
+}
 
