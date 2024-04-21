6abd96022e333ecd1ce0712d55707075179d6b77
ashwinbmhadeshwar <142456220+ashwinbmhadeshwar@users.noreply.github.com>
2024-04-21 13:26:40 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,13 +3,13 @@ import java.awt.Graphics;
 public class Game {
 
 	private Queen qL = new Queen(true);
-	private Queen qD = new Queen(false);
+	// private Queen qD = new Queen(false);
 	private Bishop bL = new Bishop(true);
-	private Bishop bD = new Bishop(false);
+	// private Bishop bD = new Bishop(false);
 	private Rook rL = new Rook(true);
-	private Rook rD = new Rook(false);
+	// private Rook rD = new Rook(false);
 	private King kL = new King(true);
-	private King kD = new King(false);
+	// private King kD = new King(false);
 	
 	private int[][] grid = new int[8][8];
 
@@ -26,9 +26,9 @@ public class Game {
 	public Game()
 	{
 //		0 = nothing ; 1 = king ; 2 = queen ; 3 = rook ; 4 = bishop
-//
-//		dark pieces
-		grid[0][0] = 3; grid[0][2] = 4; grid[0][3] = 2; grid[0][4] = 1; grid[0][5] = 4; grid[0][7] = 3;
+// //
+// //		dark pieces
+// 		grid[0][0] = 3; grid[0][2] = 4; grid[0][3] = 2; grid[0][4] = 1; grid[0][5] = 4; grid[0][7] = 3;
 
 //		light pieces
 		grid[7][0] = 3; grid[7][2] = 4; grid[7][3] = 2; grid[7][4] = 1; grid[7][5] = 4; grid[7][7] = 3;
@@ -37,7 +37,7 @@ public class Game {
 	public void draw(Graphics g)
 	{
 		qL.draw(g, grid);
-		qD.draw(g, grid);
+		// qD.draw(g, grid);
 //		bL.draw(g, grid);
 //		bD.draw(g, grid);
 //		rL.draw(g, grid);
