c2307b08682a4e2c3f4aa3c66d84b759c49b5161
ashwinbmhadeshwar <142456220+ashwinbmhadeshwar@users.noreply.github.com>
2024-04-21 11:55:54 -0400

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -49,5 +49,78 @@ public class Panel extends JPanel
 			}
 		}
 	}
+	private class KeyControl implements KeyListener
+	{
+		public void keyTyped(KeyEvent e) 
+		{
+			
+		}
+
+		public void keyPressed(KeyEvent e) 
+		{
+			String s = "";
+			switch (e.getKeyCode())
+	         {
+	            case KeyEvent.VK_A:
+	               s = "A";
+	               break;
+	            case KeyEvent.VK_B:
+	            	s = "B";
+	               break;
+	            case KeyEvent.VK_C:
+	            	s = "C";
+	               break;
+	            case KeyEvent.VK_D:
+	            	s = "D";
+	               break;
+	            case KeyEvent.VK_E: 
+	            	s = "E";
+	            	break;
+	            case KeyEvent.VK_F:
+	            	s = "F";
+	            	break;
+	            case KeyEvent.VK_G:
+	            	s = "G";
+	            	break;
+	            case KeyEvent.VK_H:
+	            	s = "H";
+	            	break;
+	            case KeyEvent.VK_1:
+	            	s = "1";
+	            	break;
+	            case KeyEvent.VK_2:
+	            	s = "2";
+	            	break;
+	            case KeyEvent.VK_3:
+	            	s = "3";
+	            	break;
+	            case KeyEvent.VK_4:
+	            	s = "4";
+	            	break;
+	            case KeyEvent.VK_5:
+	            	s = "5";
+	            	break;
+	            case KeyEvent.VK_6:
+	            	s = "6";
+	            	break;
+	            case KeyEvent.VK_7:
+	            	s = "7";
+	            	break;
+	            case KeyEvent.VK_8:
+	            	s = "8";
+	            	break; 	
+	            case KeyEvent.VK_BACK_SPACE:
+	            	s = "remove";
+	            	break;
+	         }
+			
+		}
+
+		public void keyReleased(KeyEvent e) 
+		{
+			
+		}
+		
+	}
 }
 
