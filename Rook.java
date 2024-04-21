b348db2fd567a937921d946c2fbf6c9681543350
ashwinbmhadeshwar <142456220+ashwinbmhadeshwar@users.noreply.github.com>
2024-04-21 11:16:52 -0400

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 
-public class Rook
+public class Rook extends Queen
 {
     private ImageIcon blackRook, whiteRook;
     public Rook()
     {
+        super(
         blackRook = new ImageIcon("images/DarkRook.png");
         whiteRook = new ImageIcon("images/LightRook.png");
     }
