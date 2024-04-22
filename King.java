a20a72eccda86fa0a24ec2a972825b7751a3c545
ashwinbmhadeshwar <142456220+ashwinbmhadeshwar@users.noreply.github.com>
2024-04-21 22:11:49 -0400

    Update King.java

--- King.java
+++ King.java
@@ -4,9 +4,9 @@ public class King extends Queen
 {
     private ImageIcon pieceTeam;
     private boolean team;
-    public King(boolean team)
+    public King(int x, int y, boolean team)
     {
-        super(team);
+        super(x, y, team);
         if(!team)
             pieceTeam = new ImageIcon("images/DarkKing.png");
         else
