7dd64ee435a03ce4caedf0b0e818a566556c0a2a
ashwinbmhadeshwar <142456220+ashwinbmhadeshwar@users.noreply.github.com>
2024-04-21 22:11:26 -0400

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -4,9 +4,9 @@ public class Bishop extends Queen
 {
     private ImageIcon pieceTeam;
     private boolean team;
-    public Bishop(boolean team)
+    public Bishop(boolean team, int x, int y)
     {
-        super(team);
+        super(x, y, team);
         if(!team)
             pieceTeam = new ImageIcon("images/DarkBishop.png");
         else
